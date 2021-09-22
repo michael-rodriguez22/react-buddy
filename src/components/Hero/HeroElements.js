0effbffff6763a09093a96ca98b4dfe65a00c398
Michael Rodriguez <mikeyrod22@gmail.com>
2021-09-22 18:18:43 -0500

    hero content

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -7,8 +7,42 @@ export const HeroContainer = styled.div`
   background-size: cover;
   background-attachment: fixed;
   background-position: 80%;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  align-items: flex-start;
+`;
+
+export const HeroTextWrapper = styled.div`
+  width: 50%;
+  text-align: right;
+  font-style: italic;
+  padding: 100px 0 0 50px;
+  text-shadow: 0 0 16px ${({ theme }) => theme.dark};
+
+  @media screen and (max-width: 760px) {
+    width: 100%;
+    text-align: center;
+    font-style: italic;
+    padding: 100px 0 0;
+  }
+`;
+
+export const HeroHeading = styled.h1`
+  color: ${({ theme }) => theme.primary};
+  font-size: 3.4rem;
+  font-style: italic;
+  margin-bottom: 2rem;
+
+  @media screen and (max-width: 760px) {
+    font-size: 2.4rem;
+    padding: 0 1rem;
+  }
+`;
+
+export const HeroContent = styled.p`
+  color: ${({ theme }) => theme.light};
+  font-size: 2rem;
+
+  @media screen and (max-width: 760px) {
+    font-size: 1.6rem;
+    padding: 0 3rem;
+    font-style: normal;
+  }
 `;
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,23 @@
-import { HeroContainer } from "./HeroElements";
+import {
+  HeroContainer,
+  HeroTextWrapper,
+  HeroHeading,
+  HeroContent,
+} from "./HeroElements";
 
 const Hero = () => {
   return (
     <>
-      <HeroContainer id="home"></HeroContainer>
+      <HeroContainer id="home">
+        <HeroTextWrapper>
+          <HeroHeading>What keeps you going?</HeroHeading>
+          <HeroContent>
+            We are here to help you lace up your shoes and chase what is
+            important to you. Whatever your goals are, our job is to keep you
+            running towards them.
+          </HeroContent>
+        </HeroTextWrapper>
+      </HeroContainer>
       <div style={{ height: 1000 }}></div>;
     </>
   );
